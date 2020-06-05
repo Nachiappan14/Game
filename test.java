e5882e05b4ec10fd1705d69a18d4ac0c31f2ef04
nsk <nicenach149@gmail.com>
2020-06-05 17:45:38 +0530

    The OLD default package files are just organised as a new package called"src" and the new functions added in the controller.java for the animation of the atoms moving and read function made as the property of view and now the controller has to call the ui to get input or output or animation xD

--- test.java
+++
@@ -1,9 +0,0 @@
-class test{
-    public static void main(String[] args){
-        System.out.println("Hey!!Nachiappan is the Greatest");
-        game = new controller(3, 9, 6);
-        // game.game();
-        System.out.print (game.endGame);
-    }
-    private static controller game;
-}
--- view.java
+++
@@ -1,3 +0,0 @@
-public abstract class view {
-    public abstract void render();
-}
